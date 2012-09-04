e02776b07ea6b2f167e4b92d1157f430aabe93a9
Vladimir Grinenko <Vladimir Grinenko tadatuta@yandex-team.ru>
2012-09-05 01:49:21 +0300

    initial

---
+++ .bem/make.js
@@ -0,0 +1,17 @@
+//process.env.YENV = 'production';
+
+MAKE.decl('Arch', {
+
+    getLibraries: function() {
+
+        return {
+            'bem-bl': {
+                type: 'git',
+                url: 'git://github.com/bem/bem-bl.git',
+                treeish: '0.3'
+            }
+        };
+
+    }
+
+});
---
+++ README.md
@@ -0,0 +1,7 @@
+bem server demo
+===============
+
+1. git clone git://github.com/tadatuta/yasubbotnik-bem-server.git
+2. cd yasubbotnik-bem-server
+3. npm install
+4. bem server
