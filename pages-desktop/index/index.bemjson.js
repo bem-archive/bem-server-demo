0c0a6fefeb58e31e6903529a732270120b940f7a
Vladimir Alaev <scff@yandex-team.ru>
2012-09-05 23:29:51 +0300

    - slogans added

--- pages-desktop/index/index.bemjson.js
+++ pages-desktop/index/index.bemjson.js
@@ -1,6 +1,6 @@
 ({
     block: 'b-page',
-    title: 'Bem server motivator',
+    title: 'BEM Server Motivator',
     head: [
         { elem: 'css', url: '_index.css', ie: false },
         { elem: 'css', url: '_index.ie.css', ie: 'lt IE 8' },
@@ -19,46 +19,65 @@
                                 {
                                     elem: 'item',
                                     content: [
-                                        {block: 'motivator', content: [
-                                            {elem: 'img', elemMods: {theme: 'bem'}},
-                                            {elem: 'tagline', content: 'Делай BEM!'}
+                                        {
+                                            block: 'motivator',
+                                            content: [
+                                                {elem: 'img', elemMods: {theme: 'bem'}},
+                                                {elem: 'slogan', content: 'BEM SERVER'},
+                                                {elem: 'tagline', content: 'Делай BEM!'}
                                         ]}
                                     ]
                                 },
                                 {
                                     elem: 'item',
                                     content: [
-                                        {block: 'motivator', content: [
-                                            {elem: 'img', elemMods: {theme: 'another'}},
-                                            {elem: 'tagline', content: 'Человек-летучая мышь - мрачный крепыш!'}
-                                        ]}
+                                        {
+                                            block: 'motivator',
+                                            content: [
+                                                {elem: 'img', elemMods: {theme: 'another'}},
+                                                {elem: 'slogan', content: 'BATMAN'},
+                                                {elem: 'tagline', content: 'Человек-летучая мышь - мрачный крепыш!'}
+                                            ]
+                                        }
                                     ]
                                 },
                                 {
                                     elem: 'item',
                                     content: [
-                                        {block: 'motivator', content: [
-                                            {elem: 'img', elemMods: {theme: 'america'}},
-                                            {elem: 'tagline', content: 'Капитан Америка - у всех истерика!'}
-                                        ]}
+                                        {
+                                            block: 'motivator',
+                                            content: [
+                                                {elem: 'img', elemMods: {theme: 'america'}},
+                                                {elem: 'slogan', content: 'CAPTAIN AMERICA'},
+                                                {elem: 'tagline', content: 'Капитан Америка - у всех истерика!'}
+                                            ]
+                                        }
                                     ]
                                 },
                                 {
                                     elem: 'item',
                                     content: [
-                                        {block: 'motivator', content: [
-                                            {elem: 'img', elemMods: {theme: 'iron'}},
-                                            {elem: 'tagline', content: 'Железный человек - в шлеме на век!'}
-                                        ]}
+                                        {
+                                            block: 'motivator',
+                                            content: [
+                                                {elem: 'img', elemMods: {theme: 'iron'}},
+                                                {elem: 'slogan', content: 'IRONMAN'},
+                                                {elem: 'tagline', content: 'Железный человек - в шлеме на век!'}
+                                            ]
+                                        }
                                     ]
                                 },
                                 {
                                     elem: 'item',
                                     content: [
-                                        {block: 'motivator', content: [
-                                            {elem: 'img', elemMods: {theme: 'brain'}},
-                                            {elem: 'tagline', content: 'А этого никто не знает'}
-                                        ]}
+                                        {
+                                            block: 'motivator',
+                                            content: [
+                                                {elem: 'img', elemMods: {theme: 'brain'}},
+                                                {elem: 'slogan', content: '???'},
+                                                {elem: 'tagline', content: 'А этого никто не знает'}
+                                            ]
+                                        }
                                     ]
                                 }
                             ]
