2912b447c0d07ba8aaddb65ba1bebd22a93fc4c8
Vladimir Alaev <scff@yandex-team.ru>
2012-09-05 23:18:00 +0300

    - increased min tagline height - bemjson formatting fixed

--- pages-desktop/index/index.bemjson.js
+++ pages-desktop/index/index.bemjson.js
@@ -5,9 +5,9 @@
         { elem: 'css', url: '_index.css', ie: false },
         { elem: 'css', url: '_index.ie.css', ie: 'lt IE 8' },
         { block: 'i-jquery', elem: 'core' },
-        { elem: 'js', url: '_index.js' },
+        { elem: 'js', url: '_index.js' }
     ],
-    content:[
+    content: [
         {
             block: 'content',
             content: [
@@ -18,23 +18,48 @@
                             content: [
                                 {
                                     elem: 'item',
-                                    content: [{block: 'motivator', content: [{elem: 'img', elemMods: {theme:'bem'}}, {elem: 'tagline', content: 'Делай BEM!'}]}]
+                                    content: [
+                                        {block: 'motivator', content: [
+                                            {elem: 'img', elemMods: {theme: 'bem'}},
+                                            {elem: 'tagline', content: 'Делай BEM!'}
+                                        ]}
+                                    ]
                                 },
                                 {
                                     elem: 'item',
-                                    content: [{block: 'motivator', content: [{elem: 'img', elemMods: {theme:'another'}}, {elem: 'tagline', content: 'Человек-летучая мышь - мрачный крепыш!'}]}]
+                                    content: [
+                                        {block: 'motivator', content: [
+                                            {elem: 'img', elemMods: {theme: 'another'}},
+                                            {elem: 'tagline', content: 'Человек-летучая мышь - мрачный крепыш!'}
+                                        ]}
+                                    ]
                                 },
                                 {
                                     elem: 'item',
-                                    content: [{block: 'motivator', content: [{elem: 'img', elemMods: {theme:'america'}}, {elem: 'tagline', content: 'Капитан Америка - у всех истерика!'}]}]
+                                    content: [
+                                        {block: 'motivator', content: [
+                                            {elem: 'img', elemMods: {theme: 'america'}},
+                                            {elem: 'tagline', content: 'Капитан Америка - у всех истерика!'}
+                                        ]}
+                                    ]
                                 },
                                 {
                                     elem: 'item',
-                                    content: [{block: 'motivator', content: [{elem: 'img', elemMods: {theme:'iron'}}, {elem: 'tagline', content: 'Железный человек - в шлеме на век!'}]}]
+                                    content: [
+                                        {block: 'motivator', content: [
+                                            {elem: 'img', elemMods: {theme: 'iron'}},
+                                            {elem: 'tagline', content: 'Железный человек - в шлеме на век!'}
+                                        ]}
+                                    ]
                                 },
                                 {
                                     elem: 'item',
-                                    content: [{block: 'motivator', content: [{elem: 'img', elemMods: {theme:'brain'}}, {elem: 'tagline', content: 'А этого никто не знает'}]}]
+                                    content: [
+                                        {block: 'motivator', content: [
+                                            {elem: 'img', elemMods: {theme: 'brain'}},
+                                            {elem: 'tagline', content: 'А этого никто не знает'}
+                                        ]}
+                                    ]
                                 }
                             ]
                         }
